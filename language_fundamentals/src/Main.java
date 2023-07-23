dfb0a1a9aed2258a67be69603ec7dd73b58e3dfb
bsaranga <bsaranga@gmail.com>
2023-07-23 10:58:19 +0530

    Removed some lines

--- language_fundamentals/src/Main.java
+++ language_fundamentals/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         byte foo = (byte) 128;
-        byte overflowed_foo = (byte) 256;
-
         System.out.print(foo);
-        //System.out.print(overflowed_foo);
     }
 }
