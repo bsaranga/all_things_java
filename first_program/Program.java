a9ddee79c0fb832281481cda6dcc1d7f534d05e1
bsaranga <bsaranga@gmail.com>
2023-07-23 10:49:09 +0530

    first steps

---
+++ first_program/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("I am become death, the destroyer of worlds.");
+		if (args.length > 0) {
+			System.out.println("Additional arguments" + args[0]);
+		}
+	}
+}
