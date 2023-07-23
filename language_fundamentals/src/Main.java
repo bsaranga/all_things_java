62a8c1c4133f2a01fcc8c6af0a214a0fbc54e606
bsaranga <bsaranga@gmail.com>
2023-07-23 11:18:40 +0530

    basic arrays

--- language_fundamentals/src/Main.java
+++ language_fundamentals/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        byte foo = (byte) 128;
-        System.out.print(foo);
+        int[] intArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var mapped = Arrays.stream(intArray).map(f -> f * 2);
+
+        var copy = Arrays.copyOfRange(intArray, 2, 5);
+        Arrays.stream(copy).forEach(e -> System.out.println(e));
+        mapped.forEach(m -> System.out.println(m));
     }
 }
